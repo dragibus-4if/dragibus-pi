af041ebe8173240c82561aca8782bef8b8a9a7e8
Turpin Pierre <pturpin@if501-vm01.insa-lyon.fr>
2013-09-30 11:05:02 +0200

    Création du fichier types.h

---
+++ src/types.h
@@ -0,0 +1,15 @@
+/* types.h
+ * Définie des types entiers à taille fixe.
+ */
+#define TYPES_H
+#define TYPES_H
+
+typedef char                int8_t;
+typedef int                 int16_t;
+typedef long int            int32_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned int        uint16_t;
+typedef unsigned long int   uint32_t;
+
+#endif
