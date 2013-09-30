cef1fcf2a367c7e80f72fc0a967805d52205d164
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-30 11:04:17 +0200

    Ajout du fichier source du dispatcher et de ctx_s

--- src/process.h
+++ src/process.h
@@ -1,6 +1,24 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-// TODO
+#include "types.h"
+
+/* Structure de donnée définissant un contexte:
+ *  pc : "program counter", adresse courante de l'exécution
+ *  sp : "stack pointer", pointeur de pile
+ */
+struct ctx_s {
+    uint16_t pc;
+    uint16_t sp;
+};
+
+/* Initialise un contexte */
+extern void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size);
+
+/* Démarre une fonction en utilisant un certain contexte */
+extern void start_ctx(struct ctx_s* ctx, func_t f);
+
+/* Switch à un autre contexte */
+extern void switch_to(struct ctx_s* ctx);
 
 #endif
