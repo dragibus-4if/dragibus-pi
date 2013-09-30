03e33993ea309d0790702e06bedcdd942146b1e4
Turpin Pierre <pturpin@if501-219-21.insa-lyon.fr>
2013-09-30 11:26:30 +0200

    Ajoute la constante STACK_SIZE dans le notmain.c (a changer) et le type func_t

--- src/types.h
+++ src/types.h
@@ -1,9 +1,10 @@
 /* types.h
- * Définie des types entiers à taille fixe.
  */
 #ifndef TYPES_H
 #define TYPES_H
 
+/* Définie des types entiers à taille fixe.
+ */
 typedef char                int8_t;
 typedef int                 int16_t;
 typedef long int            int32_t;
@@ -11,5 +12,9 @@ typedef long int            int32_t;
 typedef unsigned char       uint8_t;
 typedef unsigned int        uint16_t;
 typedef unsigned long int   uint32_t;
+
+/* Définie un type de pointeur de fonction
+ */
+typedef void (*func_t)(void);
 
 #endif
