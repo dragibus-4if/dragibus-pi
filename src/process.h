b90cf3ef1f93507fd2c4a902348a2924d59397d5
Turpin Pierre <pturpin@if501-219-18.insa-lyon.fr>
2013-10-07 10:29:34 +0200

    Déclare les fonctions pour utiliser l'ordonnanceur collaboratif

--- src/process.h
+++ src/process.h
@@ -20,9 +20,36 @@ struct ctx_s {
     uint32_t regs[15];
 };
 
+/* Définition d'un Process Control Block (PCB)
+ * funct    : Pointeur vers la fonction à appeler
+ * args     : Argument à donners à la fonction
+ * pc       : "Program Counter", adresse courante de l'exécution
+ * sp       : "Stack Pointer", pointeur de pile
+ * regs     : Liste des registres (r0..r15)
+ * state    : 0 si la fonction n'a pas été lancé. 1 si elle l'est. -1 si elle est arreté.
+ */
+struct pcb_s {
+    funct_t funct;
+    void * args;
+
+    uint32_t pc;
+    uint32_t sp;
+    int32_t regs[15];
+};
 
 void init_ctx ( struct ctx_s* ctx, func_t f, unsigned int stack_size );
 
 void start_ctx ( struct ctx_s* ctx, func_t f, void * args );
+
+/* Initialise un PCB */
+extern void init_pcb();
+
+extern void create_process(funct_t f, void args);
+
+extern void yield();
+
+extern void start_current_process();
+
+extern void ctx_switch();
 
 #endif
--- src/types.h
+++ src/types.h
@@ -2,27 +2,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
+
 /* Définie un type de pointeur de fonction */
-typedef void (*func_t)(void);
+typedef void (*func_t)();
 
 #ifndef NULL
 #  define NULL 0
 #endif
 
-#ifndef _GCC_STDINT_H
-#define _GCC_STDINT_H
+typedef uint8_t bool_t;
 
-/* Définie des types entiers à taille fixe. */
-typedef char                int8_t;
-typedef int                 int16_t;
-typedef long int            int32_t;
-
-typedef unsigned char       uint8_t;
-typedef unsigned int        uint16_t;
-typedef unsigned long int   uint32_t;
-
-typedef unsigned long int   size_t;
-
-#endif
+#define FALSE 0
+#define TRUE 1
 
 #endif
