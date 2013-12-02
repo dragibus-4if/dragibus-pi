ac6b56cbbf9840dfb91c361af9dc1a514f42262d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-02 21:08:14 +0100

    Merge sem.h header guards

--- src/sem.h
+++ src/sem.h
@@ -1,11 +1,17 @@
-struct pcb_Sem{
+#ifndef SEM_H
+#define SEM_H
+
+struct pcb_Sem {
     struct pcb_s* pcb;
     struct pcb_Sem* next;
 }
+
 struct sem_s {
     int32_t counter;
     struct pcb_Sem* pcbSemF;
     struct pcb_Sem* pcbSemL;
 }
 
-void sem_init(struct sem_s *sem, uint32_t val);
+void sem_init(struct sem_s* sem, uint32_t val);
+
+#endif
