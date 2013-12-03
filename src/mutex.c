a8bb4e9a2dfe641bc1f0cbf514f744ac4ae10b23
Patrizia Peller <patrizia.peller@googlemail.com>
2013-12-03 11:50:32 +0100

    free() et acquire(), mais je ne suis pas sûre si acquire() fait le bon truc

--- src/mutex.c
+++ src/mutex.c
@@ -16,6 +16,7 @@
 }*/
 
 static struct _mutex_s {
+    struct pcb_s * owner;
     struct sem_s sem;
 }
 
@@ -33,16 +34,48 @@ int mutex_create(int * desc) {
     
     /*Initialisation du mutex*/
     sem_init(&mutex->sem, 1);
+    mutex->owner = NULL;
+    *desc = (int) mutex;
     
     return 0;
 }
 
-int mutex_destroy(int desc) {
+/**
+ * Méthode privée pour changer le mutex. Renvoye -1 si desc est 0, et à si le changement s'est bien passé.
+ */
+static int _mutex_desc_convert(int desc, _mutex_s * mutex) {
+    if (desc <= 0) {
+        return -1;
+    }
+    *mutex = *(_mutex_s *) desc;
+    return 0;
+}
 
+int mutex_free(int desc) {
+    struct _mutex_s * mutex = NULL;
+    if (_mutex_desc_convert(desc, mutex) == -1) {
+        return -1;
+    }
+
+    malloc_free(mutex->sem);
+    malloc_free(mutex);
+    return 0;
 }
 
 int mutex_acquire(int desc) {
-
+    //convertir le desc en mutex
+    struct _mutex_s * mutex = NULL;
+    if (_mutex_desc_convert(desc, mutex) == -1) {
+        return -1;
+    }
+    
+    //sem down
+    sem_down(mutex->sem);
+    
+    //stocker le pcb courant dans le mutex
+    mutex->owner = current_process;
+    
+    return 0;
 }
 
 int mutex_release(int desc) {
--- src/mutex.h
+++ src/mutex.h
@@ -11,12 +11,20 @@
 
 /**
  * Création du mutex, initialize la sémaphore inclue dans le mutex. Renvoye 0 si la création
- * s'est bien finie, -1 si le desc est NULL ou bien l'allocation du mémoire a échoué.
+ * s'est bien finie, -1 si le desc est NULL ou bien l'allocation de la mémoire a échoué.
  */
 int mutex_create(int * desc);
 
-int mutex_destroy(int desc);
+/**
+ * Détruire le mutex et libérer la mémoire du mutex. Renvoye -1 si le changement du mutex a été
+ * un échec, 0 si tout va bien.
+ */
+int mutex_free(int desc);
 
+/**
+ * Bloquer un processus préciser par desc. Renvoye -1 si le mutex n'a pas pu être modifié, 0 si
+ * le processus a bien été bloqué.
+ */
 int mutex_acquire(int desc);
 
 int mutex_release(int desc);
