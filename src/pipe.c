7d23c045ef31893e614dda4b3ea506ef0ba322f4
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-03 08:41:32 +0100

    Implement pipe_create

--- src/pipe.c
+++ src/pipe.c
@@ -41,19 +41,30 @@ static int _pipe_des_to_end(int des, _pipe_end_s * pipe) {
     return 0;
 }
 
+/* Type des données */
+typedef char _buffer_t;
+
 int pipe_create(int * in_des, int * out_des) {
     struct _pipe_end_s * read_end;
     struct _pipe_end_s * write_end;
 
+    /* TODO fixer la taille de départ */
+    static const size_t bufsize = 1000;
+
+    /* Vérification des paramètres */
+    if (in_des == NULL || out_des == NULL) {
+        return -1;
+    }
+
+    /* Création du point d'entrée */
     write_end = (struct _pipe_end_s *) malloc_alloc(sizeof(struct _pipe_end_s));
     if (write_end == NULL) {
         return -1;
     }
     write_end->des = (int) write_end;
     write_end->state = WRITABLE;
-    write_end->buffer = NULL;
-    write_end->bufsize = 0;
 
+    /* Création du point de sortie */
     read_end = (struct _pipe_end_s *) malloc_alloc(sizeof(struct _pipe_end_s));
     if (read_end == NULL) {
         malloc_free(write_end);
@@ -61,10 +72,20 @@ int pipe_create(int * in_des, int * out_des) {
     }
     read_end->des = (int) read_end;
     read_end->state = READABLE;
-    read_end->buffer = NULL;
-    read_end->bufsize = 0;
 
-    /* TODO créer le pipe buffer commun */
+    /* Création du buffer commun */
+    write_end->bufsize = read_end->bufsize = bufsize;
+    write_end->buffer = read_end->buffer = return malloc_alloc(bufsize * sizeof(_buffer_t));
+    if (write_end->buffer == NULL) {
+        malloc_free(read_end);
+        malloc_free(write_end);
+        return -1;
+    }
+
+    /* Retour par paramètre */
+    *in_des = (int) read_end;
+    *out_des = (int) write_end;
+    return 0;
 }
 
 int pipe_close(int des) {
@@ -78,3 +99,5 @@ int pipe_close(int des) {
 ssize_t pipe_read(int * des, void * buffer, size_t bufsize);
 
 ssize_t pipe_write(int * des, const void * buffer, size_t bufsize);
+
+/* vim: ft=c et sw=4 sts=4 */
