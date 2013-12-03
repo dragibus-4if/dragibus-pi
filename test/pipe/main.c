5a6acb78a547431f8b99addaaa13596fa91672b6
TurpIF <pierre.turpin@insa-lyon.fr>
2013-12-03 17:16:56 +0100

    Ajout de plein de tests (qui passent trop bien)

--- test/pipe/main.c
+++ test/pipe/main.c
@@ -1,6 +1,7 @@
 #include "pipe.h"
 #include "malloc.h"
 
+/* TODO changer ça un jour (en gardant le 42) */
 #define ASSERT(cond) if(!(cond)) { *((char *)0) = 42; }
 
 int main(void) {
@@ -8,15 +9,42 @@ int main(void) {
     char mem[0x30000 - 0x8000];
     malloc_init((void * )&mem);
 
-    intptr_t in, out;
+    intptr_t readable, writable;
+    char buffer1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char buffer2[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     ASSERT(pipe_create(NULL, NULL) == -1);
-    ASSERT(pipe_create(&in, NULL) == -1);
-    ASSERT(pipe_create(NULL, &out) == -1);
+    ASSERT(pipe_create(&readable, NULL) == -1);
+    ASSERT(pipe_create(NULL, &writable) == -1);
+    ASSERT(pipe_read(NULL, NULL, 0) == -1);
+    ASSERT(pipe_write(NULL, NULL, 0) == -1);
+    ASSERT(pipe_close(NULL) == -1);
 
-    if(pipe_create(&in, &out) == 0) {
-        pipe_close(in);
-        pipe_close(out);
+    if(pipe_create(&readable, &writable) == 0) {
+        ASSERT(pipe_read(readable, NULL, 0) == -1);
+        ASSERT(pipe_read(writable, NULL, 0) == -1);
+        ASSERT(pipe_read(writable, buffer2, 0) == -1);
+        ASSERT(pipe_read(readable, buffer2, 0) == 0);
+
+        ASSERT(pipe_write(writable, NULL, 0) == -1);
+        ASSERT(pipe_write(readable, NULL, 0) == -1);
+        ASSERT(pipe_write(readable, buffer1, 0) == -1);
+        ASSERT(pipe_write(writable, buffer1, 0) == 0);
+
+        ssize_t nb_write = pipe_write(writable, buffer1, 10);
+        ssize_t nb_read = pipe_read(readable, buffer2, 1000);
+        ASSERT(nb_write != -1);
+        ASSERT(nb_read != -1);
+        ASSERT(nb_read <= 10);
+
+        ssize_t nb = nb_write > nb_read ? nb_read : nb_write;
+        int i;
+        for(i = 0 ; i < nb ; i++) {
+            ASSERT(buffer1[i] == buffer2[i]);
+        }
+
+        pipe_close(readable);
+        pipe_close(writable);
     }
     else
       return -1;
