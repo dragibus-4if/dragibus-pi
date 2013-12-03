2f15f45a438e4ff188d5ec4696889bef711f3a53
TurpIF <pierre.turpin@insa-lyon.fr>
2013-12-03 16:38:39 +0100

    int -> intptr_t

--- src/malloc.c
+++ src/malloc.c
@@ -1,13 +1,13 @@
 #include "malloc.h"
 
 struct malloc_entry_t {
-  uint32 size;
-  char   data[0];
+  uint32_t size;
+  char data[0];
 };
 
 struct malloc_heap_t {
-  uint32 size;
-  uint32 tail;
+  uint32_t size;
+  uint32_t tail;
   struct malloc_entry_t *entries[0];
 };
 
@@ -18,10 +18,10 @@ struct malloc_entry_t *malloc_get_entry(char *ptr) {
   return (struct malloc_entry_t *)(ptr - sizeof(struct malloc_entry_t));
 }
 
-void malloc_free_list_swap(uint32 x, uint32 y);
+void malloc_free_list_swap(uint32_t x, uint32_t y);
 
 void malloc_init(void *ptr) {
-  uint32 size = 20480;
+  uint32_t size = 20480;
   malloc_head = ptr;
   malloc_free_list = (struct malloc_heap_t *) malloc_head;
   malloc_head += sizeof(struct malloc_heap_t) +
@@ -32,8 +32,8 @@ void malloc_init(void *ptr) {
 
 void malloc_free(void *ptr) {
   struct malloc_entry_t *entry = malloc_get_entry(ptr);
-  uint32 curr = malloc_free_list->tail++;
-  uint32 parent = curr / 2;
+  uint32_t curr = malloc_free_list->tail++;
+  uint32_t parent = curr / 2;
   malloc_free_list->entries[curr] = entry;
   while( curr != 0 ) {
     if (
@@ -49,31 +49,31 @@ void malloc_free(void *ptr) {
   }
 }
 
-void malloc_free_list_swap(uint32 x, uint32 y) {
+void malloc_free_list_swap(uint32_t x, uint32_t y) {
   struct malloc_entry_t *tmp;
   tmp = malloc_free_list->entries[x];
   malloc_free_list->entries[x] = malloc_free_list->entries[y];
   malloc_free_list->entries[y] = tmp;
 }
 
-uint32 malloc_leq(uint32 left, uint32 right) {
+uint32_t malloc_leq(uint32_t left, uint32_t right) {
   return malloc_free_list->entries[left]->size <=
     malloc_free_list->entries[right]->size;
 }
 
-uint32 malloc_le(uint32 left, uint32 right) {
+uint32_t malloc_le(uint32_t left, uint32_t right) {
   return malloc_free_list->entries[left]->size <
     malloc_free_list->entries[right]->size;
 }
 
-void malloc_free_list_remove(uint32 curr) {
+void malloc_free_list_remove(uint32_t curr) {
   if ( malloc_free_list->tail == 0 ) {
     return;
   }
 
-  uint32 tail = --malloc_free_list->tail;
-  uint32 left  = (curr*2)+1;
-  uint32 right = left+1;
+  uint32_t tail = --malloc_free_list->tail;
+  uint32_t left  = (curr*2)+1;
+  uint32_t right = left+1;
   malloc_free_list_swap(curr, tail);
 
   while(left < tail) {
@@ -90,20 +90,20 @@ void malloc_free_list_remove(uint32 curr) {
   }
 }
 
-char *malloc_alloc_new_entry(uint32 size) {
+char *malloc_alloc_new_entry(uint32_t size) {
   struct malloc_entry_t *entry = (struct malloc_entry_t *)malloc_head;
   malloc_head += size + sizeof(struct malloc_entry_t);
   entry->size = size;
   return entry->data;
 }
 
-char* malloc_alloc(uint32 size) {
+char* malloc_alloc(uint32_t size) {
   // make all sizes 4 byte aligned
   if ( size & 0x3 ) {
     size = size - (size & 0x3) + 4;
   }
-  uint32 curr = 0;
-  uint32 tail = malloc_free_list->tail;
+  uint32_t curr = 0;
+  uint32_t tail = malloc_free_list->tail;
   char *ptr = 0;
   while( curr < tail && malloc_free_list->entries[curr]->size < size ) {
     curr = (curr*2)+1;
@@ -118,18 +118,18 @@ char* malloc_alloc(uint32 size) {
   return ptr;
 }
 
-uint32 malloc_freelist_length() {
+uint32_t malloc_freelist_length() {
   return malloc_free_list->tail;
 }
 
-void memcpy(void *src, void *dst, uint32 len) {
-  uint32 ilen = len / 4;
-  uint32 clen = len % 4;
-  uint32 *isrc = (uint32 *)src;
-  uint32 *idst = (uint32 *)dst;
-  uint8  *csrc = (uint8 *)src;
-  uint8  *cdst = (uint8 *)src;
-  uint32 i;
+void memcpy(void *src, void *dst, uint32_t len) {
+  uint32_t ilen = len / 4;
+  uint32_t clen = len % 4;
+  uint32_t *isrc = (uint32_t *)src;
+  uint32_t *idst = (uint32_t *)dst;
+  uint8_t  *csrc = (uint8_t *)src;
+  uint8_t  *cdst = (uint8_t *)src;
+  uint32_t i;
   for(i=0; i<ilen; ++i) {
     idst[i] = isrc[i];
   }
--- src/malloc.h
+++ src/malloc.h
@@ -5,8 +5,8 @@
 
 void malloc_init(void *ptr);
 void malloc_free(void *ptr);
-char* malloc_alloc(uint32 size);
-uint32 malloc_freelist_length();
-void memcpy(void *src, void *dst, uint32 len);
+char* malloc_alloc(uint32_t size);
+uint32_t malloc_freelist_length();
+void memcpy(void *src, void *dst, uint32_t len);
 
 #endif
