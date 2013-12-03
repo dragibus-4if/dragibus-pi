37b573672ff432d4bbeb4878db6cba1c25c1c036
TurpIF <pierre.turpin@insa-lyon.fr>
2013-12-03 16:50:41 +0100

    Ajout d'une macro de ouf pour assert

--- test/pipe/main.c
+++ test/pipe/main.c
@@ -1,6 +1,8 @@
 #include "pipe.h"
 #include "malloc.h"
 
+#define ASSERT(cond) if(!(cond)) { *((char *)0) = 42; }
+
 int main(void) {
     /* Initialisation de la RAM */
     char mem[0x30000 - 0x8000];
@@ -8,6 +10,11 @@ int main(void) {
     malloc_free((void *)malloc_alloc(10000));
 
     intptr_t in, out;
+
+    ASSERT(pipe_create(NULL, NULL) == -1);
+    ASSERT(pipe_create(&in, NULL) == -1);
+    ASSERT(pipe_create(NULL, &out) == -1);
+
     if(pipe_create(&in, &out) == 0) {
         pipe_close(in);
         pipe_close(out);
