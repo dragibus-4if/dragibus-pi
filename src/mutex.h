1050453265c079d779c22fc29b2db8f039cc856f
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-03 16:46:46 +0100

    More fix

--- src/mutex.h
+++ src/mutex.h
@@ -1,6 +1,8 @@
 #ifndef MUTEX_H
 #define MUTEX_H
 
+#include "types.h"
+
 /* Crée un mutex et renvoie son descripteur en modifiant *desc*. Renvoie 0 si
  * tout s'est bien passé ou -1 si une erreur a eu lieu. */
 int mutex_create(intptr_t * desc);
