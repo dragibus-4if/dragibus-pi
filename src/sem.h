eadce85fb82c871b1a477c9d8a3ce6365d11c45e
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-06 10:42:48 +0100

    Add comments

--- src/sem.h
+++ src/sem.h
@@ -3,10 +3,18 @@
 
 #include "types.h"
 
+/* Structure de données représentant une sémapore */
 struct sem_s;
 
+/* Initialize une sémaphore déjà allouée */
 void sem_init(struct sem_s * sem, int val);
+
+/* Incrémente la valeur d'une sémaphore, plaçant le processus en attente quand
+ * la valeur max a été atteinte */
 void sem_up(struct sem_s * sem);
+
+/* Décrémente la valeur d'une sémaphore, plaçant le processus en attente quand
+ * la valeur min a été atteinte */
 void sem_down(struct sem_s * sem);
 
 #endif
