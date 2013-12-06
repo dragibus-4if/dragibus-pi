48242731df557a7453b01e68ba8d5e10c3e79564
TurpIF <pierre.turpin@insa-lyon.fr>
2013-12-06 08:25:30 +0100

    removage du mutex_t et remplacement par mutex_s *

--- src/mutex.c
+++ src/mutex.c
@@ -4,51 +4,13 @@
 #include "sched.h"
 
 /* Structure de données privée du mutex */
-struct _mutex_s {
+struct mutex_s {
     struct pcb_s * owner;
     struct sem_s sem;
 };
 
-/* Index courant et max du dernier mutex créé */
-static mutex_t _current_index = -1;
-#define _max_mutex_index 1024
-
-/* Table des mutex créés */
-static struct _mutex_s * _mutex_array[_max_mutex_index] = {NULL};
-
-/* Permet de convertir le descripteur de mutex en mutex réel. Cas d'erreur :
- * - desc est négatif ou nul : adresse invalide */
-static int _mutex_desc_convert(mutex_t des, struct _mutex_s * mutex) {
-    if (des < 0 || des >= _max_mutex_index) {
-        return -1;
-    }
-    if (_mutex_array[des] == NULL) {
-        return -1;
-    }
-    *mutex = *_mutex_array[des];
-    return 0;
-}
-
-int mutex_create(mutex_t * desc) {
+int mutex_create(mutex_s * mutex) {
     /* Vérification des paramètres */
-    if (desc == NULL) {
-        return -1;
-    }
-
-    /* Cherche un index valide */
-    mutex_t mid;
-    for (mid = (_current_index + 1) % _max_mutex_index ;
-        mid != _current_index && _mutex_array[mid] != NULL ;
-        mid = (mid + 1) % _max_mutex_index);
-
-    /* Si on a pas trouvé un seul descripteur valide */
-    if (mid == _current_index) {
-      return -1;
-    }
-
-    /* Création */
-    struct _mutex_s * mutex = (struct _mutex_s *) malloc_alloc(
-        sizeof(struct _mutex_s));
     if (mutex == NULL) {
         return -1;
     }
@@ -66,22 +28,20 @@ int mutex_create(mutex_t * desc) {
     return 0;
 }
 
-int mutex_free(mutex_t desc) {
-    struct _mutex_s * mutex = NULL;
-    if (_mutex_desc_convert(desc, mutex) == -1) {
+int mutex_free(mutex_s * desc) {
+    /* Vérification des paramètres */
+    if (mutex == NULL) {
         return -1;
     }
 
-    /* TODO sem_destroy(mutex->sem); */
-    malloc_free((void *) mutex);
+    /* TODO sem_destroy(mutex->sem); ? */
     _mutex_array[desc] = NULL;
     return 0;
 }
 
-int mutex_acquire(mutex_t desc) {
-    /* Convertir le descripteur en mutex */
-    struct _mutex_s * mutex = NULL;
-    if (_mutex_desc_convert(desc, mutex) == -1) {
+int mutex_acquire(mutex_s * desc) {
+    /* Vérification des paramètres */
+    if (mutex == NULL) {
         return -1;
     }
 
@@ -95,7 +55,12 @@ int mutex_acquire(mutex_t desc) {
     return 0;
 }
 
-int mutex_release(mutex_t desc) {
+int mutex_release(mutex_s * desc) {
+    /* Vérification des paramètres */
+    if (mutex == NULL) {
+        return -1;
+    }
+
     /* TODO */
   return 0;
 }
--- src/mutex.h
+++ src/mutex.h
@@ -3,22 +3,22 @@
 
 #include "types.h"
 
-typedef int mutex_t;
+extern struct mutex_s;
 
 /* Crée un mutex et renvoie son descripteur en modifiant *desc*. Renvoie 0 si
  * tout s'est bien passé ou -1 si une erreur a eu lieu. */
-int mutex_create(mutex_t * desc);
+int mutex_create(mutex_s * desc);
 
 /* Libère le mutex défini par *desc*. Renvoie 0 si tout s'est bien passé ou -1
  * si une erreur a eu lieu. */
-int mutex_free(mutex_t desc);
+int mutex_free(mutex_s * desc);
 
 /* Vérouille le mutex défini par *desc*. Renvoie 0 si tout s'est bien passé ou
  * -1 si une erreur a eu lieu. */
-int mutex_acquire(mutex_t desc);
+int mutex_acquire(mutex_s * desc);
 
 /* Dévérouille le mutex défini par *desc*. Renvoie 0 si tout s'est bien passé
  * ou -1 si une erreur a eu lieu. */
-int mutex_release(mutex_t desc);
+int mutex_release(mutex_s * desc);
 
 #endif
