dc31c92a726e4ce6d96c763e894cea5de5f6f7fa
Julien <julien@ju-s.(none)>
2013-12-07 18:30:14 +0100

    Oubli fic

---
+++ src/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define NULL 0
+
+typedef unsigned short          uint16_t;
+typedef unsigned int            uint32_t;
+typedef unsigned char           uint8_t;
+typedef unsigned long int       size_t;
+typedef signed long int         ssize_t;
+typedef signed long long int    intptr_t;
+typedef unsigned long long int  uintptr_t;
+
+#endif
