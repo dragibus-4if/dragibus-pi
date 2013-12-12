54d1f1b536e1a0074cf9b02d636a319af483b543
Julien <julien@ju-s.(none)>
2013-12-13 00:09:58 +0100

    Essai de modification, sched fonctionne bizarrement

--- blinking_led/kernel.c
+++ blinking_led/kernel.c
@@ -8,6 +8,7 @@ void turn_on(void * a) {
     while(1) {
         led_on();
         for(int i = 0 ; i < SLEEP_TIME ; i++);
+        sched_forced_yield();    
     }
 }
 
@@ -15,14 +16,25 @@ void turn_off(void * a) {
     while(1) {
         led_off();
         for(int i = 0 ; i < SLEEP_TIME ; i++);
+        sched_forced_yield();
+    }
+}
+
+
+void mouline(void* a) {
+    while(1) {
+        for(int i = 0 ; i < SLEEP_TIME ; i++);
+        sched_forced_yield();    
     }
 }
 
 int start_kernel(void) {
     malloc_init((void *) HEAP_START);
     init_hw();
-    create_process(&turn_on, (void *) NULL, 256, SCHED_OTHER, 5);
-    create_process(&turn_off, (void *) NULL, 256, SCHED_OTHER, 5);
+    create_process(&turn_on, (void *) NULL, 256, SCHED_OTHER, 1);
+    create_process(&turn_off, (void *) NULL, 256, SCHED_OTHER, 1);
+    for (int i=0;i<1;i++){
+    create_process(&mouline, (void *) NULL, 256, SCHED_OTHER, 5);}
     sched_start();
     return 0;
 }
